e799fd6dc4c3077cd7b71d7af773d78ae9239b58
Ilkay Beydah Saglam <beydahsaglam@hotmail.com>
2023-07-01 21:35:46 +0300

    Update hello.c

--- HelloWorld/hello.c
+++ HelloWorld/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+//Merhaba Dünya: 
+//Klasik bir başlangıç projesi olan "Merhaba Dünya", C dilinde ilk programınızı yazmanızı sağlar. 
+//Program, basit bir "Merhaba Dünya" mesajını ekrana yazdırır.
+
 int main()
 {
 	printf("Hello World");
